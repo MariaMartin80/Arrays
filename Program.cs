d7d900f24d9862369af1896ab9ba07617fa5ee49
MariaMartin80 <77515936+MariaMartin80@users.noreply.github.com>
2021-02-05 11:42:23 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[] { 1, 2, 4, 5, 6, 7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25 };
+            foreach (int i in array) 
+            {
+                Console.Write("Element Value = " + i + "\n");
+            }
+            Console.ReadKey(true);
+            
+        }
+    }
+}
